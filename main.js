380800764d6faa025fd18bf323f5278e9419e85d
AhmedHassan1920 <153684911+AhmedHassan1920@users.noreply.github.com>
2024-02-06 02:48:21 +0200

    uploading

---
+++ main.js
@@ -0,0 +1,7 @@
+let normalCat = document.getElementById("normal");
+let unNormalCat = document.getElementById("unNormal");
+let audio = document.getElementById("audio");
+
+normalCat.addEventListener('click',()=>{
+    audio.play();
+})
